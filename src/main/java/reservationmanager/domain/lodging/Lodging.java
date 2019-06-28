011b7d1034e71a206fff88b9c67f846791c484f3
Keyhoh <iheyhoh@gmail.com>
2019-06-29 00:31:57 +0900

    clean: rename field and methods to be natural sentences

--- src/main/java/reservationmanager/domain/lodging/Lodging.java
+++ src/main/java/reservationmanager/domain/lodging/Lodging.java
@@ -6,36 +6,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Lodging {
-    private LocalDate startDate;
+    private LocalDate startOn;
     private int numberOfGuests;
     private int numberOfNights;
 
-    public Lodging(LocalDate startDate, int numberOfGuests, int numberOfNights) {
-        validateLodging(startDate, numberOfNights);
-        this.startDate = startDate;
+    public Lodging(LocalDate startOn, int numberOfGuests, int numberOfNights) {
+        validateInitializeArguments(startOn, numberOfNights);
+        this.startOn = startOn;
         this.numberOfGuests = numberOfGuests;
         this.numberOfNights = numberOfNights;
     }
 
-    private void validateLodging(LocalDate startDate, int numberOfNights) {
+    private void validateInitializeArguments(LocalDate startOn, int numberOfNights) {
         List<String> messageList = new ArrayList<>();
-        if (isValidNumberOfNights(numberOfNights)) {
-            messageList.add(MessageFormat.format("{0} is illegal number of nights.", numberOfNights));
+        if (maxNumberOfNightsIsUnder(numberOfNights)) {
+            messageList.add(MessageFormat.format("{0} is or under zero or over max number of nights.", numberOfNights));
         }
-        if (isValidStartDate(startDate)) {
-            messageList.add(MessageFormat.format("{0} is a past date.", startDate));
+        if (todayIsAfter(startOn)) {
+            messageList.add(MessageFormat.format("{0} is a past date.", startOn));
         }
         if (!messageList.isEmpty()) {
             throw new IllegalArgumentException(String.join(System.lineSeparator(), messageList));
         }
     }
 
-    private boolean isValidNumberOfNights(int numberOfNights) {
+    private boolean maxNumberOfNightsIsUnder(int numberOfNights) {
         var MAX_NUMBER_OF_NIGHTS = 4;
         return numberOfNights <= 0 || numberOfNights > MAX_NUMBER_OF_NIGHTS;
     }
 
-    private boolean isValidStartDate(LocalDate startDate) {
+    private boolean todayIsAfter(LocalDate startDate) {
         return LocalDate.now().isAfter(startDate);
     }
 
