49c99b1ae5305de8f52ceb2a97f6ed2c62539fed
Keyhoh <iheyhoh@gmail.com>
2019-06-29 21:07:54 +0900

    update: add name, address requirement

--- src/main/java/reservationmanager/application/service/ReservationBuilder.java
+++ src/main/java/reservationmanager/application/service/ReservationBuilder.java
@@ -1,5 +1,6 @@
 package reservationmanager.application.service;
 
+import com.google.common.base.Strings;
 import reservationmanager.domain.guest.Guest;
 import reservationmanager.domain.lodging.Lodging;
 import reservationmanager.domain.reservation.Reservation;
@@ -16,13 +17,21 @@ public class ReservationBuilder {
         return new Reservation(guest, room, lodging);
     }
 
-    public ReservationBuilder guest(Guest guest){
-        this.guest = guest;
+    public ReservationBuilder guest(String name, String address, String tel) {
+        // 業務上宿泊客の名前と連絡先は必須（無断キャンセル等）
+        if (Strings.isNullOrEmpty(name) || Strings.isNullOrEmpty(tel)) {
+            throw new IllegalArgumentException("name and tel is required.");
+        }
+        this.guest = new Guest(name, address, tel);
         return this;
     }
 
-    public ReservationBuilder lodging(Lodging lodging){
-        this.lodging = lodging;
+    public ReservationBuilder guest(String name, String tel) {
+        return guest(name, "", tel);
+    }
+
+    public ReservationBuilder lodging(LocalDate startOn, int numberOfGuests, int numberOfNights) {
+        this.lodging = new Lodging(startOn, numberOfGuests, numberOfNights);
         return this;
     }
 }
